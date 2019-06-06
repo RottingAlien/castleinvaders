5f7abe5e43eef36fe2a757560907da4571a8d950
FabioB <fabioajbatista@gmail.com>
2019-06-06 22:35:55 +0100

    Added player with movingKeys, yeahhhh

--- src/org/academiadecodigo/murlogs/castleinvaders/Main.java
+++ src/org/academiadecodigo/murlogs/castleinvaders/Main.java
@@ -1,9 +1,15 @@
 package org.academiadecodigo.murlogs.castleinvaders;
 
+import org.academiadecodigo.murlogs.castleinvaders.Characters.Player;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    new Field(800,400 );
+
+    Player player = new Player();
+    player.prepare();
 
     }
 }
