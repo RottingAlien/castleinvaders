75a1a4200ebb1ec8335246bf114b32b5e81ad39a
Alex <cradleofcorpses@gmail.com>
2019-06-06 23:23:59 +0100

    added test image for castle

--- src/org/academiadecodigo/murlogs/castleinvaders/Field.java
+++ src/org/academiadecodigo/murlogs/castleinvaders/Field.java
@@ -1,16 +1,34 @@
 package org.academiadecodigo.murlogs.castleinvaders;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Field {
 
     Rectangle screen;
+    Picture castle;
+    Rectangle sky;
 
 
-    public Field(int width, int height){
+    public Field(){
 
-        screen = new Rectangle(0,0,width,height);
+
+        screen = new Rectangle(0,0,800,600);
         screen.draw();
+
+        sky = new Rectangle(0,0,800,600);
+        sky.setColor(new Color(150,150,200));
+        sky.fill();
+
+
+        castle = new Picture(0,0,"castletest.jpg");
+        castle.draw();
+
+
+
+
+
     }
 
 }
