ed1410e64030a64ab62e4d141db8480e7d14944f
rafaelcosta <rafael1fcosta@gmail.com>
2019-06-07 14:18:40 +0100

    changed simple grx to 800x600 canvas

--- src/org/academiadecodigo/murlogs/castleinvaders/Characters/Enemy.java
+++ src/org/academiadecodigo/murlogs/castleinvaders/Characters/Enemy.java
@@ -36,10 +36,12 @@ public class Enemy extends Character {
         if (randomSpawn == 1) {
 
             this.getPic().translate(randomX + 760, enemyY);
+            this.getPic().draw();
             return;
         }
 
         this.getPic().translate(-randomX, enemyY);
+        this.getPic().draw();
 
 
     }
@@ -52,18 +54,18 @@ public class Enemy extends Character {
         //int randomStop = (int) (Math.random() * 10);
 
         //keep them moving to the center
-        if (getPic().getX() == -10) {
+        /*if (getPic().getX() == -10) {
             getPic().draw();
-        }
+        }*/
         if (getPic().getX() < 330) {
             getPic().translate(1, 0);
             return;
         }
 
 
-        if (getPic().getX() == 760) {
+        /*if (getPic().getX() == 760) {
             getPic().draw();
-        }
+        }*/
         if (getPic().getX() > 380) {
             getPic().translate(-1, 0);
         }
