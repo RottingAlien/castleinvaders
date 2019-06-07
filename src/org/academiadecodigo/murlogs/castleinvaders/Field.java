57d18ef9e31faa65fd7b460f06dddded579b51ee
RottingAlien <cradleofcorpses@gmail.com>
2019-06-07 01:40:36 +0100

    Added and implemented graphics for Castle ,castledoor, playerdoor, sky, and 'vignette' in field

--- src/org/academiadecodigo/murlogs/castleinvaders/Field.java
+++ src/org/academiadecodigo/murlogs/castleinvaders/Field.java
@@ -5,33 +5,37 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Field {
 
-    Rectangle screen;
-    Picture castle;
-    Picture sky;
-    Picture playerDoor;
-    Picture castleDoor;
-    Picture vignette;
+    private Rectangle screen;
+    private Picture castle;
+    private Picture sky;
+    private Picture playerDoor;
+    private Picture castleDoor;
+    private Picture vignette;
 
 
     public Field(){
 
-
+        //Creates "screen area"
         screen = new Rectangle(0,0,800,600);
         screen.draw();
 
+        //Creates Sky
         sky = new Picture(0,0,"sky.png");
         sky.draw();
 
-
+        //Creates Castle
         castle = new Picture(0,0,"castle.png");
         castle.draw();
 
+        //Creates Player Door
         playerDoor = new Picture(68,122,"playerdoor.png");
         playerDoor.draw();
 
+        //Creates Castle Door
         castleDoor = new Picture(335,486,"castledoor.png");
         castleDoor.draw();
 
+        //Creates "vignette" to hide enemy spawn"
         vignette = new Picture(0,0,"vignette.png");
         vignette.draw();
 
