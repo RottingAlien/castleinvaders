f803c1e899b950d21763ba4f97e2562086567def
miguelmendes <m.mendes1996@gmail.com>
2019-06-07 18:02:53 +0100

    Added sound feature

--- src/org/academiadecodigo/murlogs/castleinvaders/Game.java
+++ src/org/academiadecodigo/murlogs/castleinvaders/Game.java
@@ -3,10 +3,12 @@ package org.academiadecodigo.murlogs.castleinvaders;
 import org.academiadecodigo.murlogs.castleinvaders.Characters.Enemy;
 import org.academiadecodigo.murlogs.castleinvaders.Characters.EnemyFactory;
 import org.academiadecodigo.murlogs.castleinvaders.Characters.Player;
+import org.academiadecodigo.murlogs.castleinvaders.Sound.Sound;
 
 import java.util.LinkedList;
 
 public class Game {
+
 
     public void startGame() throws InterruptedException {
 
@@ -20,6 +22,15 @@ public class Game {
         door.drawDoor();
 
 
+        // Start of music implementation //
+
+        String filePath = "/assets/Mega Rust.wav";
+        Sound sound = new Sound(filePath);
+        sound.play(true);
+        sound.loopIndef();
+
+        // End of music implementation //
+
 
         //number of wave -> to increase enemy creation
 
@@ -30,26 +41,52 @@ public class Game {
 
         while (true) {
 
+
+// End of background music implementation
+
+
             Thread.sleep(10);
 
             for (Enemy enemy : enemies) {
                 enemy.move(1, 0);
                 field.drawVignette();
-                if (enemy.isAtDoor() && !door.isDestroyed()){
+                if (enemy.isAtDoor() && !door.isDestroyed()) {
                     enemy.punchDoor(door);
                 }
             }
-
-
-
-
-
-
-
-
-
         }
 
     }
 
 }
+
+
+
+
+
+/*
+
+    // Start of music implementation //
+    String filePath = "/assets/Mega Rust.wav";
+    Sound sound = new Sound(filePath); // open stream
+
+            try {
+
+                    Thread.sleep(0); // wait 0 seconds between each loop
+                    sound.play(true); // play sound from start
+                    sound.loopIndef(); // play forever
+                    Thread.sleep(20000000);
+
+
+
+
+                    } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+
+                    } finally {
+                    sound.close(); // close stream
+                    }
+
+// End of background music implementation
+
+*/
