7b00486c3f9d5b3d931e37cca7f68ccb93711d6b
miguelmendes <m.mendes1996@gmail.com>
2019-06-08 18:22:15 +0100

    added background to instructions menu

--- src/org/academiadecodigo/murlogs/castleinvaders/Menu/Help.java
+++ src/org/academiadecodigo/murlogs/castleinvaders/Menu/Help.java
@@ -8,18 +8,20 @@ public class Help {
 
     private Rectangle screen;
     private Picture keywords;
+    private Picture instructionsBackground;
     private boolean helpIsCreated;
 
     public void createHelpSection() {
 
 
         screen = new Rectangle(0, 0, 800, 600);
-        keywords = new Picture(70, 180, "instructions.png");
-
+        keywords = new Picture(30, 180, "instructions.png");
+        instructionsBackground = new Picture(70,100, "menu instructions.png" );
 
         screen.draw();
-        screen.setColor(Color.LIGHT_GRAY);
-        screen.fill();
+
+       instructionsBackground.draw();
+      instructionsBackground.grow(85,180);
 
         keywords.draw();
 
@@ -30,6 +32,7 @@ public class Help {
     public void deleteHelpSection() {
         screen.delete();
         keywords.delete();
+        instructionsBackground.delete();
         helpIsCreated = false;
     }
 
