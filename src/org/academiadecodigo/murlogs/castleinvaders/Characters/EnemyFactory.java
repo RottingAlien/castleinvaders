22f52dd9ab88c043122172c4822a340283f2109d
Alex <cradleofcorpses@gmail.com>
2019-06-10 19:39:20 +0100

    Added probabilities to enemy factory

--- src/org/academiadecodigo/murlogs/castleinvaders/Characters/EnemyFactory.java
+++ src/org/academiadecodigo/murlogs/castleinvaders/Characters/EnemyFactory.java
@@ -8,15 +8,26 @@ public abstract class EnemyFactory {
 
         int randomSpawn = (int) (Math.random() * 2);
         int randomX = (int) (Math.random() * 250);
-        int randomEnemy = (int) (Math.random() * 2);
+
+        int randomEnemy = -1;
+        boolean doorEnemyProb = (Math.random() < 0.2f);
+        boolean enemyShooterProb = (Math.random() < 0.5f);
+
+        if (doorEnemyProb == true) {
+            randomEnemy = 0;
+        }
+        if (enemyShooterProb == true) {
+            randomEnemy = 1;
+        }
+
 
         switch (randomEnemy) {
             case 0:
                 return new DoorEnemy(1, randomSpawn, randomX);
             case 1:
                 return new EnemyShooter(1, randomSpawn, randomX);
+            default:
+                return new DoorEnemy(1, randomSpawn, randomX);
         }
-
-        return null;
     }
 }
