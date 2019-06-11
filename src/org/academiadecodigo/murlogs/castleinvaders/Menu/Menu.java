96794806d94be79db03d9999170c14532fd50bc0
miguelmendes <m.mendes1996@gmail.com>
2019-06-11 19:20:40 +0100

    alterations to game keys's image, correction of a glitch in help section, and implement HelpSection as a inner class of Menu class

--- src/org/academiadecodigo/murlogs/castleinvaders/Menu/Menu.java
+++ src/org/academiadecodigo/murlogs/castleinvaders/Menu/Menu.java
@@ -175,16 +175,16 @@ public class Menu implements KeyboardHandler {
                         deleteMenu();
                         return;
                     }
-                    if (selecter.getY() == 440) {
+                    if(selecter.getY()==440) {
                         helpSection.createHelpSection();
 
                         return;
                     }
-
-                    if (selecter.getY() == 520) {
-                        System.exit(0);
-                    }
                 }
+                if (selecter.getY()==520 && !helpSection.helpIsCreated()){
+                    System.exit(0);
+                }
+
                 return;
 
             case KeyboardEvent.KEY_ESC:
@@ -202,4 +202,65 @@ public class Menu implements KeyboardHandler {
     public void keyReleased(KeyboardEvent keyboardEvent) {
 
     }
+
+
+
+    public class Help {
+
+
+        private Rectangle screen;
+        private Picture keywords;
+
+        private Picture instructionsBackground;
+
+
+
+        private boolean helpIsCreated;
+
+        public void createHelpSection() {
+
+
+
+
+
+            screen = new Rectangle(0, 0, 800, 600);
+
+            screen.draw();
+            screen.setColor(Color.LIGHT_GRAY);
+            screen.fill();
+
+            keywords = new Picture(25, 60, "game keys.png");
+            instructionsBackground = new Picture(70, 100, "menu instructions.png");
+
+            keywords.grow(0,-40);
+
+
+            screen.draw();
+
+            instructionsBackground.draw();
+            instructionsBackground.grow(85, 180);
+
+
+            keywords.draw();
+
+            helpIsCreated = true;
+
+
+        }
+
+        public void deleteHelpSection() {
+            screen.delete();
+            keywords.delete();
+            instructionsBackground.delete();
+            helpIsCreated = false;
+        }
+
+        public boolean helpIsCreated() {
+            return helpIsCreated;
+        }
+
+
+
+    }
+
 }
